acef92db687ead997a11c6053e8489f4c5a10ec9
Vishal Shinde <vishals10020@gmail.com>
2026-06-22 10:40:49 +0530

    Add

--- Arrays/Que1.java
+++ Arrays/Que1.java
@@ -1,3 +1,19 @@
+
+// Question 1: Given an integer array nums, return true if any value appears at least twice in the 
+// array, and return false if every element is distinct.
+
+// Example 1:
+// Input:nums = [1, 2, 3, 1]
+// Output: true
+
+// Example 2:
+// Input: nums = [1, 2, 3, 4]
+// Output: false
+
+
+
+
+
 public class Que1{
    public static boolean FindRepNo(int nums[]){
       for(int i=0; i<nums.length-1; i++){
@@ -15,3 +31,9 @@ public class Que1{
       System.out.println(FindRepNo(nums));
    }
 }
+
+
+
+
+
+
