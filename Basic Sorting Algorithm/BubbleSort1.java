6987b32e6916127bb97ad416872a351e15f5cbec
Vishal Shinde <vishals10020@gmail.com>
2026-07-01 21:19:52 +0530

    Add Selection Sort algorithm

--- Basic Sorting Algorithm/BubbleSort1.java
+++ Basic Sorting Algorithm/BubbleSort1.java
@@ -1,17 +1,20 @@
 public class BubbleSort1{
    public static void BubbleSort(int arr[]){
       for(int turn = 0; turn < arr.length-1; turn++){
+         int swap = 0;
          for(int j = 0; j < arr.length-1-turn; j++){
             if(arr[j] > arr[j+1]){
                // swap
                int temp = arr[j];
                arr[j] = arr[j+1];
                arr[j+1] = temp; 
+               swap++;
+               System.out.print(swap+"");
             }
-
          }
       }
    }
+
    public static void printArr(int arr[]){
       for(int i = 0; i < arr.length; i++){
          System.out.print(arr[i]+" ");
@@ -26,3 +29,6 @@ public class BubbleSort1{
       
    }
 }
+
+
+// time complexity =  O(n^2) ----->not that much good more better solutions are present 
