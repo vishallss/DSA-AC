caaf91e575c04f13f6d343e4314324135f16e64f
Vishal Shinde <vishals10020@gmail.com>
2026-07-02 14:58:14 +0530

    Add Insertion Sort algorithm

---
+++ Basic Sorting Algorithm/InsertionSort.java
@@ -0,0 +1,32 @@
+public class InsertionSort{
+   public static void insertionSort(int arr[]){
+      for(int i=1; i<arr.length; i++){
+         int curr = arr[i];
+         int prev = i-1;
+         //Finding out the correct position to insert
+         while(prev >= 0 && arr[prev] > curr){
+            arr[prev + 1] = arr[prev];
+            prev--;
+         }
+         //inserrtion
+         arr[prev + 1] = curr;
+      }
+   }
+
+   public static void printArr(int arr[]){
+      for(int i = 0; i < arr.length; i++){
+         System.out.print(arr[i]+" ");
+      }
+      System.out.println();
+   }
+   public static void main(String [] args){
+      int arr[] = {5, 4, 1, 3, 2};
+   
+      insertionSort(arr);
+      printArr(arr);
+      
+   }
+}
+
+
+// time complexity =  O(n^2) ----->not that much good more better solutions are present 
